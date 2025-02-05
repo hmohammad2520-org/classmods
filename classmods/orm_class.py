bb2e382adbea3b95f859b39af80a07db1fc443ad
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-02-05 10:07:32 +0330

    Changed names to local varible names

--- classmods/orm_class.py
+++ classmods/orm_class.py
@@ -13,25 +13,26 @@ class ORMDescriptorType:
             raw_class: Type,
             constructor: Type | Callable[[Any], Any] = None,
     ) -> None:
-        self.raw_class = raw_class
-        self.constructor = constructor
+
+        self._raw_class = raw_class
+        self._constructor = constructor
 
     def to_python(self, value: Any) -> Any:
-        if self.constructor is not None:
-            return self.constructor(value)
+        if self._constructor is not None:
+            return self._constructor(value)
 
-        elif isinstance(value, self.raw_class):
+        elif isinstance(value, self._raw_class):
             return value
 
         else:
-            return self.raw_class(value)
+            return self._raw_class(value)
 
     def validate(self, value: Any) -> bool:
-        return isinstance(value, self.raw_class)
+        return isinstance(value, self._raw_class)
 
     @property
     def expected_type(self) -> Type:
-        return self.raw_class
+        return self._raw_class
 
 
 class ORMDescriptor:
@@ -54,65 +55,65 @@ class ORMDescriptor:
         if remover is None and nullable:
             raise ValueError('Remover cannot be `None` when nullable is `True`.')
 
-        self.type = type
-        self.getter = getter if not sensitive else None
-        self.setter = setter
-        self.remover = remover
-        self.validator = validator
-        self.cache_timeout = cache_timeout
-        self.changeable = changeable
-        self.nullable = nullable
-        self.sensitive = sensitive
-        self.name = ""
+        self._type = type
+        self._getter = getter if not sensitive else None
+        self._setter = setter
+        self._remover = remover
+        self._validator = validator
+        self._cache_timeout = cache_timeout
+        self._changeable = changeable
+        self._nullable = nullable
+        self._sensitive = sensitive
+        self._name = ""
 
     def __set_name__(self, owner, name: str) -> None:
-        self.name = name
+        self._name = name
 
     def __get__(self, instance: Optional[ORMClass], _) -> Any:
         if instance is None:
             return self
 
-        if self.sensitive:
-            raise AttributeError(f"Access to {self.name} is restricted. Data marked as sensetive")  # Prevent access
+        if self._sensitive:
+            raise AttributeError(f"Access to {self._name} is restricted. Data marked as sensetive")  # Prevent access
 
-        cache_entry = instance._attribute_cache.get(self.name)
-        if cache_entry and (time.time() - cache_entry[1] <= self.cache_timeout):
+        cache_entry = instance._attribute_cache.get(self._name)
+        if cache_entry and (time.time() - cache_entry[1] <= self._cache_timeout):
             return cache_entry[0]
 
-        if self.getter is None:
-            raise AttributeError(f"{self.name} does not have a getter function.")
+        if self._getter is None:
+            raise AttributeError(f"{self._name} does not have a getter function.")
 
-        raw_value = self.getter(instance)
-        value = self.type.to_python(raw_value)
+        raw_value = self._getter(instance)
+        value = self._type.to_python(raw_value)
 
-        if self.cache_timeout > 0:
-            instance._attribute_cache[self.name] = (value, time.time())
+        if self._cache_timeout > 0:
+            instance._attribute_cache[self._name] = (value, time.time())
 
         return value
 
     def __set__(self, instance: ORMClass, value: Any) -> None:
-        if not self.changeable:
-            raise AttributeError(f"{self.name} is read-only.")
+        if not self._changeable:
+            raise AttributeError(f"{self._name} is read-only.")
 
         if value is None:
-            if not self.nullable:
-                raise AttributeError(f"{self.name} is not nullable.")
-            if self.remover:
-                self.remover(instance, value)
+            if not self._nullable:
+                raise AttributeError(f"{self._name} is not nullable.")
+            if self._remover:
+                self._remover(instance, value)
 
         else:
-            if not self.type.validate(value):
-                raise ValueError(f"Invalid value type for {self.name}: {value.__class__} -> expected: {self.type.raw_class}")
+            if not self._type.validate(value):
+                raise ValueError(f"Invalid value type for {self._name}: {value.__class__} -> expected: {self._type._raw_class}")
 
-            if self.validator and not self.validator(value):
-                raise ValueError(f"Custom validation failed for {self.name}")
+            if self._validator and not self._validator(value):
+                raise ValueError(f"Custom validation failed for {self._name}")
 
-            if self.setter:
-                self.setter(instance, value)
+            if self._setter:
+                self._setter(instance, value)
 
-        instance._attribute_cache.pop(self.name, None)
+        instance._attribute_cache.pop(self._name, None)
 
     def __delete__(self, instance: ORMClass) -> None:
-        if self.remover:
-            self.remover(instance, self.name)
-        instance._attribute_cache.pop(self.name, None)
+        if self._remover:
+            self._remover(instance, self._name)
+        instance._attribute_cache.pop(self._name, None)
