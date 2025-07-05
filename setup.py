a3b52e694abf2b8cb46ef57c7968f1a98c3a2a0e
Mohammad Heydari <Hmohammad2520@gmail.com>
2025-07-05 15:05:16 +0330

    `Bump version numbers to 0.0.3 in classmods package`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='classmods',
-    version='0.0.2',
+    version='0.0.3',
     license="MIT",
     description='Simple mods for python classes.',
     author='Radin-System',
