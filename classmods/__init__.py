8654063d54193f72efcd2b4c7d7f0a7ab0ae7045
Mohammad Heydari <Hmohammad2520@gmail.com>
2025-07-05 15:04:30 +0330

    moved logwrap to decorators

--- classmods/__init__.py
+++ classmods/__init__.py
@@ -1,10 +1,12 @@
 # __version__
 from .__version__ import __version__ as version
 
+# Decorators
 from ._decorators import (
     return_exception_on_error,
     return_true_on_error,
     return_false_on_error,
+    logwrap,
 )
 
 # Live Attrib
@@ -17,21 +19,17 @@ from ._remote_attrib import (
 # Method_Spy
 from ._method_monitor import MethodMonitor
 
-# Decorators
-from ._logwrap import logwrap
-
 __all__ = [
     'version',
 
     'return_exception_on_error',
     'return_true_on_error',
     'return_false_on_error',
+    'logwrap',
 
     'RemoteAttribMixin',
     'RemoteAttrib',
     'RemoteAttribType',
 
     'MethodMonitor',
-
-    'logwrap',
 ]
