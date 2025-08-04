c52e332a9f82a2a9ecb90b56c2c666b133a81553
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-08-04 13:02:14 +0330

    Version 1.1.0

--- classmods/__version__.py
+++ classmods/__version__.py
@@ -1,9 +1,7 @@
-__version__ = '1.0.0'
-
+__version__ = '1.1.0'
 
 def get_version() -> str:
     return __version__
-
 
 __all__ = [
     '__all__',
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='classmods',
-    version='1.0.0',
+    version='1.1.0',
     license="MIT",
     description='Simple mods for python classes.',
     author='hmohammad2520-org',
     author_email='hmohammad2520@gmail.com',
     url='https://github.com/hmohammad2520-org/classmods/',
     install_requires=[],
-    packages=find_packages(exclude=['test', 'test.*']),
+    packages=find_packages(exclude=['test', 'test.*', 'dev', 'dev.*']),
     include_package_data=True,
     zip_safe=False,
 )
