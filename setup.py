9445171b3ba39c6528c8841f6165257461d251a6
Mohammad Heydari <Hmohammad2520@gmail.com>
2026-01-31 20:08:52 +0330

    Version 1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='classmods',
-    version='1.2.1',
+    version='1.2.2',
     license="MIT",
     description='Simple mods for python classes.',
     author='hmohammad2520-org',
